381faf4fcdea78b0f46276d2ce54bd31388ecf77
Denis5201 <builukden@yandex.ru>
2023-02-24 13:30:26 +0700

    Init

---
+++ app/src/main/java/com/example/dencalculatorcompose/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.example.dencalculatorcompose.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val backgroundColor = Color(0xFF1D1B1E)
+val inputColor = Color(0xFFD0BCFF)
+val errorColor = Color(0xFFF2B8B5)
+val gray = Color(0xFF4A4458)
+val red = Color(0xFF633B48)
+val onGray = Color(0xFFE8DEF8)
+val onRed = Color(0xFFFFD8E4)
+val whiteGray = Color(0xFFE6E1E5)
---
+++ app/src/main/java/com/example/dencalculatorcompose/ui/theme/Type.kt
@@ -0,0 +1,44 @@
+package com.example.dencalculatorcompose.ui.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.dencalculatorcompose.R
+
+val googleSans = FontFamily(
+    Font(resId = R.font.google_sans_regular)
+)
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    body1 = TextStyle(
+        fontFamily = googleSans,
+        fontWeight = FontWeight.Medium,
+        fontSize = 32.sp
+    ),
+    h1 = TextStyle(
+        fontFamily = googleSans,
+        fontWeight = FontWeight.Medium,
+        fontSize = 57.sp
+    ),
+    h2 = TextStyle(
+        fontFamily = googleSans,
+        fontWeight = FontWeight.Bold,
+        fontSize = 28.sp
+    )
+    /* Other default text styles to override
+    button = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp
+    ),
+    caption = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    )
+    */
+)
